13f52e0e5c5d04dedddba88d62e76b159b336e24
Henri <henri.ducrocq@gmail.com>
2015-02-08 02:19:41 +0000

    x

--- py_shell/command.py
+++ py_shell/command.py
@@ -1,33 +1,32 @@
 import subprocess
 import re
+from types import MethodType
 from pprint import pprint
 
 
 class Command:
-    count = 0
-
-    @classmethod
-    def create(cls, binary, options=None, parse_usage=False):
-
-
-    def __init__(self, binary, options=None, parse_usage=False):
-        self.binary = binary
-        if not (options or parse_usage) or (options and parse_usage):
-            raise ValueError("provide constructor with one of: parse_usage, options")
-        if parse_usage:
-            usage = self._capture_output([binary, "--help"])
-            self.options = self._parse_usage(usage)
+    def __init__(self, binary=None, options=None, parse_usage=False, pred=None, args=[]):
+        if pred:
+            self.binary = pred.binary
+            self.options = pred.options
+            self._register_options()
+            self.args = list(args)
         else:
-            self.options = options
-        self.state_class = Command._create_state_class(self)
+            self.binary = binary
+            if not (options or parse_usage) or (options and parse_usage):
+                raise ValueError("provide constructor with one of: parse_usage, options")
+            if parse_usage:
+                usage = self._capture_output([binary, "--help"])
+                self.options = self._parse_usage(usage)
+            else:
+                self.options = options
+            self._register_options()
+            self.args = []
 
-    def params(self):
-        return self.state_class(self)
-
-    def run(self, args):
+    def run(self):
         argv = [self.binary]
-        argv.extend(args)
-        print "CommandState: running: %s" % " ".join(argv)
+        argv.extend(self.args)
+        print "running: %s" % " ".join(argv)
 
     def pipe(self, other):
         # run myself, capture outputs
@@ -77,70 +76,36 @@ class Command:
             for switch in switches:
                 options[switch] = (actual_switch, help, value)
 
-        #pprint(options)
         return options
 
     def _capture_output(self, argv):
         return subprocess.Popen(argv, stdout=subprocess.PIPE).communicate()[0]
 
-    @classmethod
-    def _create_state_class(cls, cmd):
-        options = cmd.options
-        class_name = Command._make_class_name(cmd)
-        state_class = type(class_name, (CommandState,), {})
-        Command._register_options(state_class, options)
-        return state_class
-
-    @classmethod
-    def _make_class_name(cls, cmd):
-        Command.count += 1
-        class_name = "CommandState%i" % Command.count
-        return class_name
-
-    @classmethod
-    def _make_class_name0(cls, cmd):
-        if cmd.__class__ == Command:
-            basename = re.sub("^.*/", "", cmd.binary)
-            camel_cased = "".join([s.capitalize() for s in re.split("[-_.]", basename)])
-            name = camel_cased
-        else:
-            name = cmd.__class__.__name__
-        class_name = "%sCommandState" % name
-        if class_name in Command.state_classes:  #FIXME
-            raise ValueError("bug: make up unique name: %s" % class_name)
-        return class_name
-
-    @classmethod
-    def _register_options(cls, klass, options):
-        for switch, actual_and_help_and_value in options.items():
+    def _register_options(self):
+        for switch, actual_and_help_and_value in self.options.items():
             actual_switch, help, value = actual_and_help_and_value
-            Command._register_option(klass, switch, actual_switch, help, value)
+            Command._register_option(self, switch, actual_switch, help, value)
 
-    @classmethod
-    def _register_option(cls, klass, switch, actual_switch, help, value):
+    def _register_option(self, switch, actual_switch, help, value):
         def add_option(self, arg=None):
             print "stating", switch
-            self.args.append(actual_switch)
             if value and not arg:
                 raise ValueError("missing parameter for option %s" % switch)
             if arg and not value:
-                raise ValueError("option %s does not take parameter, was givent: %s" % (actual_switch, arg))
+                raise ValueError("option %s does not take parameter, was given: %s" % (actual_switch, arg))
+            args = list(self.args)
+            args.append(actual_switch)
             if arg:
-                self.args.append(arg)
-            return self
+                args.append(arg)
+            return Command(pred=self, args=args)
         add_option.__doc__ = help
         add_option.__name__ = switch
-        setattr(klass, add_option.__name__, add_option)
-
-
-class CommandState(object):
-    def __init__(self, command):
-        self.command = command
-        self.args = []
+        method = MethodType(add_option, self)
+        setattr(self, add_option.__name__, method)
 
     def arg(self, arg):
         self.args.append(arg)
         return self
 
     def run(self):
-        self.command.run(self.args)
+        print "running ", self.binary, self.args
--- test/test_command.py
+++ test/test_command.py
@@ -4,31 +4,60 @@ import unittest
 from py_shell.command import Command
 #import subprocess
 #from pprint import pprint
+from types import MethodType
 
 
 class TestCommand(unittest.TestCase):
-    def test_1(self):
-        ls = Command("/usr/bin/ls", parse_usage=True)
-        ls.params().literal().run()
-        ls = Command("ls", parse_usage=True)
-        ls.params().literal().run()
-
     def test_2(self):
         ls = LsCommand()
-        ls.literal().run()
+        ls2 = ls.literal()
+        print "\nls.run():"
+        #ls.literal().run()
+        ls.help().run()
+        #ls.literal().literal().literal().run()
+        print "\nls2.run():"
+        ls2.run()
+        #print "\nls2.run():"
+        #ls2.run()
+        #print "\nls.help().run():"
+        #ls.help().run()
+        print "\nls2.help().run():"
+        ls2.help().run()
 
+        #rsync = RsyncCommand()
+        #rsync.run()
+        fail
+
+    def xtest_copy_methods(self):
+        t = Thing()
+
+        def oi(self):
+            print "hey"
+        oi.__name__ = "hey"
+
+        m = MethodType(oi, t)
+        setattr(t, "hey", m)
+
+        t.hey()
+
+        ###########
+        t2 = Thing()
+
+        m = t.__dict__["hey"]
+        print m
+        t2.__dict__["hey"] = m
+
+        t2.hey()
+        fail
+
+
+class Thing:
+    pass
 
 class LsCommand(Command):
     def __init__(self):
-        Command.__init__(self)
+        Command.__init__(self, binary="ls", parse_usage=True)
 
-    def run(self):
-        pass
-
-    def literal(self):
-        print "parsing literal"
-
-
-class LsCommandState(CommandState):
-    def literal(self):
-        self.sup.literal()
+class RsyncCommand(Command):
+    def __init__(self):
+        Command.__init__(self, binary="rsync", parse_usage=True)
